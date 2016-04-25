34bc53fb20f21963413a58a5db2e1d2899f12b5d
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-04-25 19:29:05 -0400

    Implement 'clear_' method

--- lib/mail_form/base.rb
+++ lib/mail_form/base.rb
@@ -1,7 +1,22 @@
 module MailForm
     class Base
+        include ActiveModel::AttributeMethods # attribute methods behavior
+        attribute_method_prefix 'clear_' # clear_ is attribute prefix
+
         def self.attributes(*names)
             attr_accessor(*names)
+
+            # Ask to define the prefix methods for the given attribute names
+            define_attribute_methods(names)
         end
+
+        protected
+
+            # Since "clear_" prefix was declared, it expects to have a
+            # "clear_attribute" method defined, which receives an attribute
+            # name and implements the clearing logic.
+            def clear_attribute(attr)
+                send("#{attr}=", nil)
+            end
     end
 end
--- test/mail_form_test.rb
+++ test/mail_form_test.rb
@@ -4,9 +4,15 @@ require 'fixtures/sample_mail'
 class MailFormTest < ActiveSupport::TestCase
     test 'sample mail has name and email as attributes' do
         sample = SampleMail.new
+
         sample.name = 'User'
         assert_equal 'User', sample.name
         sample.email = 'user@example.com'
         assert_equal 'user@example.com', sample.email
+
+        sample.clear_name
+        sample.clear_email
+        assert_nil sample.name
+        assert_nil sample.email
     end
 end
