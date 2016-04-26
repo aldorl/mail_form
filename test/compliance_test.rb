0873f1e5c007cc346d6d3589f9511d0229c4a7ee
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-04-25 21:00:25 -0400

    Test for internationalization

--- test/compliance_test.rb
+++ test/compliance_test.rb
@@ -12,4 +12,21 @@ class ComplianceTest < ActiveSupport::TestCase
         assert_equal "sample_mail", @model.class.model_name.singular
         assert_equal "Sample mail", @model.class.model_name.human
     end
+
+    test "model_name.human uses I18n" do
+        begin
+            I18n.available_locales = [:en, :es]
+
+            I18n.backend.store_translations :en,
+                activemodel: { models: { sample_mail: "My Sample Mail" } }
+            assert_equal "My Sample Mail", @model.class.model_name.human
+
+            I18n.locale = :es
+            I18n.backend.store_translations :es,
+                activemodel: { models: { sample_mail: "Mi Correo Muestra" } }
+            assert_equal "Mi Correo Muestra", @model.class.model_name.human
+        ensure
+            I18n.reload!
+        end
+    end
 end
