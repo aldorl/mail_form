f547fc3b4fe8223c483cb3e70ccb02b4f54a8f41
Aldo Ruiz <aldo.ruiz.luna@gmail.com>
2016-04-26 00:29:44 -0400

    Implement navigation test with capybara

--- test/dummy/config/routes.rb
+++ test/dummy/config/routes.rb
@@ -5,6 +5,9 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
+  resources :contact_forms, only: :create
+  root to: "contact_forms#new"
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
